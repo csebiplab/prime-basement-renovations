4f30b89f20653d41a2fcf06a2e353a64b95f29e7
Kongkon Jowarder <kongkon4545@gmail.com>
2024-11-17 02:07:18 +0600

    remove before componets

--- src/components/__one_time_used/MainHomePage/MainHomePage.jsx
+++ src/components/__one_time_used/MainHomePage/MainHomePage.jsx
@@ -1,24 +1,11 @@
 import React from "react";
 import "./MainHomePage.css";
 import AboutUs from "./AboutUs/AboutUs";
-import LeadingToronto from "./LeadingToronto/LeadingToronto";
-import TrustedPartner from "./TrustedPartner/TrustedPartner";
-import DesignCustomization from "./DesignCustomization/DesignCustomization";
-import MaterialsEquipment from "./MaterialsEquipment/MaterialsEquipment";
-import Specialists from "./Specialists/Specialists";
-import HireFor from "./HireFor/HireFor";
 import WorkingProcess from "./WorkingProcess/WorkingProcess";
-import ConsiderationsLimitations from "./ConsiderationsLimitations/ConsiderationsLimitations";
 import Banner from "./Banner/Banner";
-import { bHeadingTexts } from "@/utils/bHeadingText";
-import { headingIconText } from "@/utils/heading-text";
 import PartnerNRatings from "./PartnerNRatings/PartnerNRatings";
-import OurServiceAreas from "./OurServiceAreas/OurServiceAreas";
 import ProjectGallery from "./ProjectGallery/ProjectGallery";
-import CostFactors from "./CostFactors/CostFactors";
-import InstallationServices from "./InstallationServices/InstallationServices";
 import Faq from "./Faq/Faq";
-import JoinWithUs from "./JoinWithUs/JoinWithUs";
 import Benefits from "./Benefits/Benefits";
 import WhyChooseUs from "./WhyChooseUs/WhyChooseUs";
 import ContactUs from "./ContactUs/ContactUs";
@@ -94,41 +81,9 @@ const MainHomePage = () => {
         <ReadyToStart />
       </section>
 
-      {/* <section>
-        <LeadingToronto />
-      </section> */}
-
-      {/* <section>
-        <TrustedPartner />
-      </section> */}
-
-      {/* <section>
-        <DesignCustomization />
-      </section> */}
-
-      {/* <section>
-        <MaterialsEquipment />
-      </section> */}
-
-      {/* <section>
-        <Specialists />
-      </section> */}
-
-      {/* <section>
-        <InstallationServices />
-      </section> */}
-
       <section>
         <BasementRenovationsContractors />
       </section>
-
-      
-
-      
-
-      {/* <section>
-        <ConsiderationsLimitations />
-      </section> */}
 
       <section>
         <Benefits />
@@ -137,20 +92,6 @@ const MainHomePage = () => {
       <section>
         <Review />
       </section>
-
-      {/* <section>
-        <CostFactors />
-      </section> */}
-
-      
-
-      {/* <section>
-        <HireFor />
-      </section> */}
-
-      {/* <section>
-        <JoinWithUs />
-      </section> */}
 
       <section>
         <Faq />
@@ -163,14 +104,6 @@ const MainHomePage = () => {
       <section>
         <ContactNow />
       </section>
-
-      {/* <section className="padding__all bg_poolInstallationCompanyInMarkham">
-        <OurServiceAreas
-          classNames={{ bg: "bg_poolInstallationCompanyInMarkham" }}
-          sHeading={headingIconText.map__IconTxt}
-          bHeading={bHeadingTexts.homePageLocationMap__bHeading}
-        />
-      </section> */}
 
       {/* <section>
         <ContactUs/>
